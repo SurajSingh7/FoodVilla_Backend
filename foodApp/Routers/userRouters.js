8133c8a6a96526d204147f06b66b160d996e3dff
SurajSingh7 <220suraj@gmail.com>
2023-04-28 20:27:09 +0530

    User operation done

--- foodApp/Routers/userRouters.js
+++ foodApp/Routers/userRouters.js
@@ -1,28 +1,36 @@
 const express=require('express');
 const userRouter=express.Router();
 const {protectRoute}=require('./authHelper');
-const {getuser,getUsers,postuser,pathuser,deleteuser,getUserById}=require('../controller/userController');
+const {getUser,updateUser,deleteUser,getAllUser}=require('../controller/userController');
 
-userRouter
-  .route('/')
-  .get(protectRoute,getUsers)
-  .post(postuser)
-  .patch(pathuser)
-  .delete(deleteuser)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // userRouter
-//   .route('/getcookies')
-//   .get(getcookies);
+//   .route('/')
+//   .get(protectRoute,getUsers)
+//   .post(postuser)
+//   .patch(pathuser)
+//   .delete(deleteuser)
 
 // userRouter
-//  .route('/setcookies')
-//  .get(setcookies);      
- 
- 
-userRouter
- .route('/:id')
- .get(getUserById); 
+//  .route('/:id')
+//  .get(getUserById); 
 
 
 module.exports=userRouter;
--- foodApp/controller/userController.js
+++ foodApp/controller/userController.js
@@ -1,59 +1,88 @@
 const userModel=require('../models/userModels');
 
 
-module.exports.getuser=function getuser(req,res){
-    res.send("hi");
-    // res.sendFile("D:/full stack wd/fjp-1/Backend/public/index.html");
+module.exports.getUser=async function getUser(req,res){
+
+    try {
+        let id = req.params.id;
+        let user = await userModel.findById(id);
+    
+        res.json({ msg: "users retrieved", user });
+      } catch (err) {
+        res.json({
+          msg: err.message,
+        });
+      }
 };
 
-module.exports.getUsers=async function getUsers(req,res){
-
-    // read all data from model(in database)
-    // let allUsers=await userModel.find(); 
-    // read all data on the basis of query from model(in database)
-    // let allUsers=await userModel.find({name:'suraj'}); 
-    
-    // read 1st data from model(in database)
-    // let allUsers=await userModel.findOne(); 
-    // read 1st data on the basis of query from model(in database)
-     let allUsers=await userModel.find(); 
-    
-     res.json({mess:"yes",data:allUsers});
-     console.log(allUsers);
-  }
-
-  module.exports.postuser=function postuser (req,res){
+module.exports.updateUser = async function (req, res) {
     console.log(req.body);
-    users=req.body;
-    res.json({
-        mess:"successfully",
-        user:req.body
-    })
-};
-
-module.exports.pathuser=function pathuser(req,res){
-    console.log(req.body);
-    // update data in user object hi 
+    let id = req.params.id;
+    let user = await userModel.findById(id);
     let dataToBeUpdated = req.body;
-    for (key in dataToBeUpdated) {
-        users[key] = dataToBeUpdated[key];
+    // {
+    //   name: "Abhi",
+    //   email: 'abc@gmail.com'
+    // }
+    try {
+      if (user) {
+
+        for (key in dataToBeUpdated) {
+            user[key] = dataToBeUpdated[key];
+          }
+
+        console.log("abcd ",user);
+  
+        const updatedData = await user.save();
+        res.json({
+          message: "data updated succesfully",
+          updatedData,
+        });
+      } else {
+        res.json({
+          message: "user not found",
+        });
+      }
+
     }
-    res.json({
-        message: "data updated succesfully"
-    })
+    catch (err) {
+      res.json({
+        message: err.message,
+      });
+    }
+
+  };
+
+module.exports.deleteUser=async function deleteUser(req,res){
+    try {
+        let id = req.params.id;
+        // let doc = await userModel.deleteOne({ email: "abcd@gmail.com" });
+        // let doc = await userModel.findOneAndRemove({ email: "abcde@gmail.com" });
+        let user = await userModel.findByIdAndDelete(id);
+        res.json({
+          msg: "user has been deleted",
+          user,
+        });
+      } catch (err) {
+        res.json({
+          msg: err.message,
+        });
+      }
+
 };
 
-module.exports.deleteuser=function deleteuser(req,res){
-    users={};
 
-    res.json({
-      message: "data has been deleted"
-  })
-
-};
-
-module.exports.getUserById=function getUserById(req,res){
-    console.log(req.params.id);    // 2
-    res.json({msg:"user id is ","obj":req.params}); // id:2
-};
+module.exports.getAllUser = async function (req, res) {
+    try {
+      let allUsers = await userModel.find();
+      res.json({
+        msg: "user id is ",
+        allUsers,
+      });
+    } catch (err) {
+      res.json({
+        msg: err.message,
+      });
+    }
+  };
 
