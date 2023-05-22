3475d3450e81c627bb6fe78ae1a20d24347dd751
SurajSingh7 <220suraj@gmail.com>
2023-05-22 11:17:55 +0530

    PlanRouters done

---
+++ foodApp/Routers/planRouters.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const { protectRoute, isAuthorised } = require('../helper');
+const planRouter = express.Router();
+const { getAllPlans, getPlan, createPlan, updatePlan, deletePlan, top3Plans } = require('../controller/planController');
+
+planRouter
+    .route('/all')
+    .get(getAllPlans);
+
+planRouter
+    .route('/top3')
+    .get(top3Plans)
+
+planRouter.use(protectRoute) //logged in hai ya nhi 
+planRouter
+    .route('/single/:id')
+    .get(getPlan)
+
+planRouter.use(isAuthorised(['admin', 'restaurantowner'])) // logged in , lekin role kya hai
+planRouter
+    .route("/crud")
+    .post(createPlan);
+
+planRouter
+    .route('/crud/:id')
+    .patch(updatePlan)
+    .delete(deletePlan)
+
+module.exports = planRouter;
--- foodApp/app.js
+++ foodApp/app.js
@@ -9,8 +9,12 @@ app.use(express.json()); // middleware fucn => post, front->json
 const cookieParser=require('cookie-parser');
 app.use(cookieParser());
 
-const userRouter=require('./Routers/userRouters');
-app.use('/user',userRouter);
 
-const planModel=require('./models/planModels');
+const userRouter=require('./Routers/userRouters');
+const planRouter = require('./Routers/planRouters');
+
+app.use('/user',userRouter);
+app.use('/plan',planRouter);
+
+
 
