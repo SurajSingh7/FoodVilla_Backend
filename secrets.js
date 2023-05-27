2e7e012e5eaf9d74e4eb3666a6b366769cd6b6e7
SurajSingh7 <220suraj@gmail.com>
2023-05-27 21:09:46 +0530

    check

---
+++ secrets.js
@@ -0,0 +1,5 @@
+
+const db_link='mongodb+srv://220suraj:220surajM@cluster0.cpbzobw.mongodb.net/?retryWrites=true&w=majority';
+const  jwt_key='gufdytstsrsaesgfhfyfy';
+const  mailPass="ztodjyebnddmdkrs";
+module.exports= {db_link,jwt_key,mailPass};
